8e98b1168fc1badcd2a3d708aa7628d4ffcd3696
zenin5479 <zenin5479@gmail.com>
2026-03-10 12:04:11 +0300

    Update 0.1 Обновлен метод. Проведен рефакторинг

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,16 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(800, 450);
-         this.Text = "Form1";
+         SuspendLayout();
+         // 
+         // Form1
+         // 
+         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         ClientSize = new System.Drawing.Size(800, 450);
+         Name = "Form1";
+         Text = "Console в WinForms ";
+         ResumeLayout(false);
       }
 
       #endregion
