4b08809ace5205d12386da72db6f5ecce3f75a61
zenin5479 <zenin5479@gmail.com>
2026-03-10 13:45:40 +0300

    Update 1.2 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -71,7 +71,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(351, 282);
+         ClientSize = new System.Drawing.Size(274, 282);
          Controls.Add(lblResult);
          Controls.Add(btnCalculate);
          Controls.Add(txtResult);
