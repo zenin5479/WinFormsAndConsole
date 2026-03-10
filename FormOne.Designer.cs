463d28036445ecf90e472ec8f3d374856a96a9d7
zenin5479 <zenin5479@gmail.com>
2026-03-10 12:14:18 +0300

    Update 0.8 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -34,7 +34,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(519, 433);
+         ClientSize = new System.Drawing.Size(484, 361);
          Name = "FormOne";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Console в WinForms";
